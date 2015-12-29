2c263bffb3cdac415b24a275a1235dffc7a9f329
Dhruvdutt Jadhav <jdhruv12@gmail.com>
2015-12-29 16:07:47 +0530

    connected to mongolabs

--- model/db.js
+++ model/db.js
@@ -1,2 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/ejs');
+mongoose.connect('mongodb://dhruvdutt:dhruvdutt@ds061474.mongolab.com:61474/dhruvdutt');
+//mongoose.connect('mongodb://localhost/ejs');
